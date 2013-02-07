8fba505fe0d3c44415cf181204e95f805d2fe31b
Mairin Chesney <chesne14@msu.edu>
2013-02-07 15:33:10 -0500

    Test

---
+++ TestProject/gen/edu/cse/msu/chesne14/testproject/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package edu.cse.msu.chesne14.testproject;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ TestProject/gen/edu/cse/msu/testproject/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package edu.cse.msu.testproject;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
