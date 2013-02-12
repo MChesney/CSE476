7e8d4808047eccfc9e0b8775fe4be63791ad78b8
Mairin Chesney <chesne14@msu.edu>
2013-02-12 10:07:52 -0500

    Fixed up layouts a little bit.

--- DroidDraw/gen/edu/msu/cse/boggle/droiddraw/R.java
+++ DroidDraw/gen/edu/msu/cse/boggle/droiddraw/R.java
@@ -12,16 +12,20 @@ public final class R {
     }
     public static final class drawable {
         public static final int android=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int android_transparent=0x7f020001;
+        public static final int colors=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
     }
     public static final class id {
-        public static final int androidImage=0x7f070005;
-        public static final int menu_settings=0x7f070006;
-        public static final int playerOneEdit=0x7f070003;
-        public static final int playerOneText=0x7f070004;
-        public static final int playerTwoEdit=0x7f070002;
-        public static final int playerTwoText=0x7f070001;
-        public static final int startGame=0x7f070000;
+        public static final int androidImage=0x7f070000;
+        public static final int buttonLayout=0x7f070005;
+        public static final int instructions=0x7f070007;
+        public static final int menu_settings=0x7f070008;
+        public static final int playerOneEdit=0x7f070001;
+        public static final int playerOneText=0x7f070002;
+        public static final int playerTwoEdit=0x7f070004;
+        public static final int playerTwoText=0x7f070003;
+        public static final int startGame=0x7f070006;
     }
     public static final class layout {
         public static final int activity_edit=0x7f030000;
@@ -37,6 +41,7 @@ public final class R {
         public static final int android_description=0x7f04000a;
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040001;
+        public static final int instructions=0x7f04000b;
         public static final int menu_settings=0x7f040002;
         public static final int player_one=0x7f040005;
         public static final int player_one_edit_hint=0x7f040007;
--- DroidDraw/src/edu/msu/cse/boggle/droiddraw/OpeningActivity.java
+++ DroidDraw/src/edu/msu/cse/boggle/droiddraw/OpeningActivity.java
@@ -2,7 +2,10 @@ package edu.msu.cse.boggle.droiddraw;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
 
 public class OpeningActivity extends Activity {
 
@@ -17,6 +20,38 @@ public class OpeningActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_opening, menu);
 		return true;
+	}
+	
+	public void onStartGame(View view) {
+		EditText one = (EditText) findViewById(R.id.playerOneEdit);
+		EditText two = (EditText) findViewById(R.id.playerTwoEdit);
+		
+		String playerOneName = one.getText().toString();
+		String playerTwoName = two.getText().toString();
+		
+		if (playerOneName.length() == 0) {
+			one.requestFocus();
+		} else if (playerTwoName.length() == 0) {
+			two.requestFocus();
+		} else {
+
+		}
+		
+	}
+	
+	public void onInstructions(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
+        
+        // Parameterize the builder
+        //builder.setTitle(R.string.hurrah);
+        //builder.setMessage(R.string.completed_puzzle);
+        builder.setPositiveButton(android.R.string.ok, null);
+		//builder.setNegativeButton(R.string.shuffle, listener);
+        
+        // Create the dialog box and show it
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+		
 	}
 	
 }
