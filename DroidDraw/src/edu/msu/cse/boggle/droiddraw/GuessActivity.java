4999c2d8cbb48c7dae64bbaaa4e6c1aed7641448
Sana <siddiq39@msu.edu>
2013-02-28 14:38:32 -0500

    more guess stuff

--- DroidDraw/src/edu/msu/cse/boggle/droiddraw/GuessActivity.java
+++ DroidDraw/src/edu/msu/cse/boggle/droiddraw/GuessActivity.java
@@ -1,8 +1,10 @@
 package edu.msu.cse.boggle.droiddraw;
 
-import android.media.AudioManager;
+
+import java.util.Locale;
+
 import android.media.MediaPlayer;
-import android.media.SoundPool;
+
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.app.Activity;
@@ -11,7 +13,6 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-//import android.widget.EditText;
 import android.widget.TextView;
 
 public class GuessActivity extends Activity {
@@ -28,9 +29,11 @@ public class GuessActivity extends Activity {
 	Button exit;
 	Button draw;
 	private CountDownTimer cdTimer;
-	//private Integer count=1;
-	//MediaPlayer mp = MediaPlayer.create(this, R.raw.lose);
 	
+	TextView playerOne;
+	TextView playerTwo ;
+	String playerOneInfo ;
+	String playerTwoInfo;
 	/**
 	 * The player information 
 	 */
@@ -47,10 +50,13 @@ public class GuessActivity extends Activity {
 		drawView = (DrawView) this.findViewById(R.id.drawViewGuess);
 		
 		Bundle infoFromPrevActivity = getIntent().getExtras();
+		answerText=(TextView) this.findViewById(R.id.Answer);
 		if (bundle != null) {
 			drawView.loadView(bundle);
 			players.loadPlayers(bundle);
-			//hint=bundle.getString("clue");
+			
+				totaltime=bundle.getLong("time");
+			    answerText.setText(bundle.getCharSequence("ans"));
 			
 		} else if (infoFromPrevActivity != null) {
 			drawView.loadView(infoFromPrevActivity);
@@ -62,54 +68,60 @@ public class GuessActivity extends Activity {
 		drawView.setEditable(false);
 		hint=this.getIntent().getExtras().getString("clue");
 		answer=this.getIntent().getExtras().getString("answer");
-		TextView playerOne = (TextView) this.findViewById(R.id.playerOne);
-		TextView playerTwo = (TextView) this.findViewById(R.id.playerTwo);
+		playerOne = (TextView) this.findViewById(R.id.playerOne);
+		playerTwo = (TextView) this.findViewById(R.id.playerTwo);
 		TextView categoryText = (TextView) this.findViewById(R.id.category);
-		String playerOneInfo = players.getName(Players.PLAYERONE) + ": " + players.getScore(Players.PLAYERONE);
-		String playerTwoInfo = players.getName(Players.PLAYERTWO) + ": " + players.getScore(Players.PLAYERTWO);
+		playerupdate();
 		
-		playerOne.setText(playerOneInfo);
-		playerTwo.setText(playerTwoInfo);
 		categoryText.setText(category);
 		 edit=(EditText) findViewById(R.id.editText1);
 		 guess= (Button)findViewById(R.id.guess);
-		 exit= (Button)findViewById(R.id.button1);
+		 exit= (Button)findViewById(R.id.finish);
 		 draw= (Button)findViewById(R.id.drawagain);
 		 draw.setEnabled(false);
+		 exit.setEnabled(false);
 		 hintdisplay = (TextView) this.findViewById(R.id.clue);
 		 timerText = (TextView) this.findViewById(R.id.time);  
-		 answerText=(TextView) this.findViewById(R.id.Answer);  
-		 if (players.getEditor()==1)
-			{
-			exit.setEnabled(false);
-			players.setEditor(2);
-			}
+		 //answerText=(TextView) this.findViewById(R.id.Answer);
+		// bundle.putInt("editor", players.getEditor());
 		
-     	else
-     		players.setEditor(1);
 		
-		 final MediaPlayer mp = MediaPlayer.create(getApplicationContext(), R.raw.lose);
+     //	else
+     	//	players.setEditor(1);
+		
+		// final MediaPlayer mp = MediaPlayer.create(getApplicationContext(), R.raw.lose);
      	
 	      cdTimer=  new CountDownTimer(totaltime, 1000) {  
+	    	  
 	            public void onTick(long m) {  
+	            	
 	            totaltime= m;
-	            timerText.setText("Time Remaining: "+m/1000);
+	            
+	            timerText.setText("Time Left: "+m/1000);
 	            answerText.setText("");
 	             if(m/1000 <=60)
 	            {
-	            	hintdisplay.setText(hint);
+	            	hintdisplay.setText("Clue: "+hint);
 	            }
-	            
+	            	
 	            }
 	            public void onFinish() {  
-	            	timerText.setText("Done!");
 	            	
-	            	answerText.setText(answer);
-	            	mp.start();
-		            
+	            	
+	            	if (!(answerText.getText().toString()).equals("YOU WIN!"))
+	            		//{
+	            		answerText.setText("Answer: "+ answer);
+	            		//if (!(timerText.getText().toString()).equals("Time up!"))
+	            			        //    	mp.start();
+	            		//}
+	            	timerText.setText("Done!");
 	            	edit.setEnabled(false);
 	            	 guess.setEnabled(false);
 	            	 draw.setEnabled(true);
+	            	 if (players.getEditor()==2)
+	     			{
+	     			exit.setEnabled(true);
+	     			}
 	            }  
 	        }.start(); 
 	}
@@ -120,10 +132,29 @@ public class GuessActivity extends Activity {
 		getMenuInflater().inflate(R.menu.activity_guess, menu);
 		return true;
 	}
-
+	public void playerupdate()
+	{
+		playerOneInfo = players.getName(Players.PLAYERONE) + ": " + players.getScore(Players.PLAYERONE);
+		playerTwoInfo = players.getName(Players.PLAYERTWO) + ": " + players.getScore(Players.PLAYERTWO);
+		
+		playerOne.setText(playerOneInfo);
+		playerTwo.setText(playerTwoInfo);
+	}
+	public void cancel(long a)
+	{
+		if (a<0)
+			cdTimer.cancel();
+	}
 	public void onDrawPicture(View view){
 		Intent intent = new Intent(this, EditActivity.class);
 		Bundle bundle = new Bundle();
+		if (players.getEditor()==2)
+    	{
+    		
+			players.setEditor(1);
+    	}
+    	else
+    		players.setEditor(2);
 		players.savePlayers(bundle);
 		intent.putExtras(bundle);
 		startActivity(intent);
@@ -141,26 +172,33 @@ public class GuessActivity extends Activity {
 	}
 	
 	public void onGuessButton(View view){
-		if((edit.getText().toString().toLowerCase()).equals(answer.toLowerCase()))
+		if((edit.getText().toString().toLowerCase(Locale.getDefault())).equals(answer.toLowerCase(Locale.getDefault())))
 		{
 			final MediaPlayer mp = MediaPlayer.create(getApplicationContext(), R.raw.win);
 			mp.start();
+			
 			timerText.setText("Done!");
 			cdTimer.cancel();
         	answerText.setText("YOU WIN!");
         	edit.setEnabled(false);
         	guess.setEnabled(false);
         	 draw.setEnabled(true);
-        	if (players.getEditor()==1)
+        	if (players.getEditor()==2)
         	{
         		players.setScore("Players.one", (int)totaltime/100);
+        		exit.setEnabled(true);
+        		
         	}
         	else
         		players.setScore("Players.two", (int)totaltime/100);
+        	totaltime=0;
+        	playerupdate();
 		}
 		else
 		{
 			edit.setText("");
+			
+	
 			answerText.setText("Wrong");
 		}
 		
@@ -170,6 +208,11 @@ public class GuessActivity extends Activity {
 	protected void onSaveInstanceState(Bundle outState) {
 		super.onSaveInstanceState(outState);
 		
+		//if((answerText.getText().toString()).equals(""))	
+			outState.putLong("time", totaltime);
+		outState.putCharSequence("ans", answerText.getText());
+		//else
+			//outState.putLong("time", -100);
         drawView.saveView(outState);
         players.savePlayers(outState);
 	}
