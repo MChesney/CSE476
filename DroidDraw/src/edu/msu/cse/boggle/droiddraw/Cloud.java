f673b9b5b553eaf7dcf02b4487f628ee5766b01e
Phil Getzen <getzenph@gmail.com>
2013-04-04 19:35:23 -0400

    Fixed the new user button. Still working on accepting input.

--- DroidDraw/src/edu/msu/cse/boggle/droiddraw/Cloud.java
+++ DroidDraw/src/edu/msu/cse/boggle/droiddraw/Cloud.java
@@ -1,5 +1,24 @@
 package edu.msu.cse.boggle.droiddraw;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.StringWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlSerializer;
+
+import android.util.Log;
+import android.util.Xml;
+
 public class Cloud {
 
     private static final String MAGIC = "NechAtHa6RuzeR8x";
@@ -29,4 +48,125 @@ public class Cloud {
 	
 	
 
+    public boolean XMLParser(InputStream stream){
+    	/**
+         * Create an XML parser for the result
+         */
+        try {
+            XmlPullParser xmlR = Xml.newPullParser();
+            xmlR.setInput(stream, UTF8);
+            
+            xmlR.nextTag();      // Advance to first tag
+            xmlR.require(XmlPullParser.START_TAG, null, "teamcranium");
+            
+            String status = xmlR.getAttributeValue(null, "status");
+            if(status.equals("no")) {
+                return false;
+            }
+            
+            // We are done
+        } catch(XmlPullParserException ex) {
+            return false;
+        } catch(IOException ex) {
+            return false;
+        }
+        return true;
+    }
+    
+    public boolean addUser (String username, String password){
+    	
+    	XmlSerializer xml = Xml.newSerializer();
+        StringWriter writer = new StringWriter();
+        
+        try {
+            xml.setOutput(writer);
+            
+            xml.startDocument("UTF-8", true);
+            
+            xml.startTag(null, "teamcranium");
+            xml.attribute(null, "user", username);
+            xml.attribute(null, "magic", MAGIC);
+            xml.attribute(null, "pw", password);
+            xml.endTag(null, "teamcranium");
+            
+            xml.endDocument();
+
+        } catch (IOException e) {
+            // This won't occur when writing to a string
+            return false;
+        }
+        
+        final String xmlStr = writer.toString();
+        
+        /*
+         * Convert the XML into HTTP POST data
+         */
+        String postDataStr;
+        try {
+            postDataStr = "xml=" + URLEncoder.encode(xmlStr, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            return false;
+        }
+        
+        /*
+         * Send the data to the server
+         */
+        byte[] postData = postDataStr.getBytes();
+    	
+    	if(postData.toString().equals(""))
+    		return false;
+    	
+    	InputStream stream = null;
+        try {
+            URL url = new URL(USER_ADD_URL);
+
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+            conn.setDoOutput(true);
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Length", Integer.toString(postData.length));
+            conn.setUseCaches(false);
+
+            OutputStream out = conn.getOutputStream();
+            out.write(postData);
+            out.close();
+
+            int responseCode = conn.getResponseCode();
+            if(responseCode != HttpURLConnection.HTTP_OK) {
+                return false;
+            } 
+            
+            stream = conn.getInputStream();
+//            logStream(stream);
+            
+            return XMLParser(stream);
+            
+        } catch (MalformedURLException e) {
+            return false;
+        } catch (IOException ex) {
+            return false;
+        } finally {
+            if(stream != null) {
+                try {
+                    stream.close();
+                } catch(IOException ex) {
+                    // Fail silently
+                }
+            }
+        }
+    }
+    
+    public static void logStream(InputStream stream) {
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(stream));
+    
+        try {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                Log.e("476", line);
+            }
+        } catch (IOException ex) {
+            return;
+        }
+    }
 }
--- DroidDraw/src/edu/msu/cse/boggle/droiddraw/CloudOpeningActivity.java
+++ DroidDraw/src/edu/msu/cse/boggle/droiddraw/CloudOpeningActivity.java
@@ -1,14 +1,24 @@
 package edu.msu.cse.boggle.droiddraw;
 
+import java.io.Serializable;
+
 import com.google.android.gcm.GCMRegistrar;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.text.InputType;
 import android.view.Menu;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class CloudOpeningActivity extends Activity {
+	
+	private Cloud cloud = new Cloud();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -44,18 +54,81 @@ public class CloudOpeningActivity extends Activity {
 		startActivity(intent);
 		
 	}
-	
-	public void onNew(View view) {
+	public void onNewUser(View view) {
+		final AlertDialog.Builder alert_builder = new AlertDialog.Builder(this);
 		
-		Intent intent = new Intent(this,  NewActivity.class);
-		startActivity(intent);
+		LinearLayout layout = new LinearLayout(this);
+		layout.setOrientation(1); //Vertical
+		
+		final EditText username = new EditText(this);
+		final EditText password = new EditText(this);
+		
+		username.setHint("");
+		password.setHint("");
+		
+		username.setInputType(InputType.TYPE_TEXT_FLAG_CAP_SENTENCES);
+		username.setActivated(true);
+		password.setInputType(InputType.TYPE_TEXT_FLAG_CAP_SENTENCES);
+		
+		final TextView usernameTitle = new TextView(this);
+		final TextView passwordTitle = new TextView(this);
+		
+		usernameTitle.setText("Username");
+		passwordTitle.setText("Password");
+		
+		layout.addView(usernameTitle);
+		layout.addView(username);
+		layout.addView(passwordTitle);
+		layout.addView(password);
+		
+		alert_builder.setView(layout);
+		
+		alert_builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+			
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				params.usernameText = username.getText().toString();
+				params.passwordText = password.getText().toString();
+				
+				cloud.addUser(params.usernameText, params.passwordText);
+				
+//				if(params.nameText.equals("") || params.hintText.equals("")){
+//					Toast.makeText(getApplicationContext(), "You must set a title and a hint", Toast.LENGTH_SHORT).show();
+//				}
+//				else{
+//					Intent intent = new Intent(getBaseContext(), GuessingActivity.class);
+//					Bundle b = new Bundle();
+//					drawingView.saveDrawing(b);
+//					intent.putExtras(b);
+//					intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//					intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//					intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+//					startActivity(intent);
+//				}
+			}
+		});
+		alert_builder.setCancelable(false);
+		alert_builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+			
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				dialog.cancel();
+			}
+		});
+		alert_builder.show();
 	}
 	
-public void onLogin(View view) {
-		
-		Intent intent = new Intent(this,  LoginActivity.class);
-		startActivity(intent);
-		
+	private static class Parameters implements Serializable {
+
+		private static final long serialVersionUID = 1L;
+
+		private String usernameText;
+		private String passwordText;
+
 	}
+    /**
+     * The current parameters
+     */
+    private Parameters params = new Parameters();
 
 }
