6f956d937b1ca2b53794a694e531dc09ea69ea40
Mairin Chesney <chesne14@msu.edu>
2013-04-03 21:59:40 -0400

    added new dialog box classes for login and new user. also added a bit of GCM stuff.

--- DroidDraw/src/edu/msu/cse/boggle/droiddraw/Cloud.java
+++ DroidDraw/src/edu/msu/cse/boggle/droiddraw/Cloud.java
@@ -5,11 +5,12 @@ public class Cloud {
     private static final String MAGIC = "NechAtHa6RuzeR8x";
     private static final String USER = "chesne14";
     private static final String PASSWORD = "drowssap";
+    private static String GCMID = "";
     
     // I believe these two are done
-    // To add user - USER_ADD_URL + "?user=" + USER + "&magic=" + MAGIC + "&pw=" + PASSWORD;
+    // To add user - USER_ADD_URL + "?user=" + USER + "&magic=" + MAGIC + "&pw=" + PASSWORD  + $gcm + GCMID; 
     private static final String USER_ADD_URL = "https://www.cse.msu.edu/~chesne14/teamcranium/user-add.php";
-    // To login user - USER_LOGIN_URL + "?user=" + USER + "&magic=" + MAGIC + "&pw=" + PASSWORD;
+    // To login user - USER_LOGIN_URL + "?user=" + USER + "&magic=" + MAGIC + "&pw=" + PASSWORD + $gcm + GCMID;
     private static final String USER_LOGIN_URL = "https://www.cse.msu.edu/~chesne14/teamcranium/user-login.php";
     
     // I haven't touched these two yet
@@ -20,5 +21,12 @@ public class Cloud {
 	public Cloud() {
 		// TODO Auto-generated constructor stub
 	}
+	
+	// This might be a really bad way to do this...
+	public static void setGcmId(String id) {
+		GCMID = id;
+	}
+	
+	
 
 }
---
+++ DroidDraw/src/edu/msu/cse/boggle/droiddraw/NewUserDlg.java
@@ -0,0 +1,11 @@
+package edu.msu.cse.boggle.droiddraw;
+
+import android.support.v4.app.DialogFragment;
+
+public class NewUserDlg extends DialogFragment {
+
+	public NewUserDlg() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
